585a9d67664af9217a0a1c7da696bdfc23b68953
Dmitry <dimkaa_@mail.ru>
2019-04-22 21:51:58 +0300

    Это первый комит

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var leftBorderWidth = 1; // видна везде
+let second = 2; //видна только в блоке
+const p = 3.14;
+
+// let number = 25;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// let person = {
+// 	name: p,
+// 	age: number,
+// 	isMarried: false
+// };
+
+// console.log(person["age"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// // console.log(arr[0]);
+
+// // let answer = confirm("Are you here?")
+// // console.log(answer);
+
+// let answer = prompt("How old of you?", "")
+// console.log(typeof (answer));
+
+let incr = 10,
+	decr = 10;
+
+console.log(incr++);
+console.log(decr++);
